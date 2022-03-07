d431919f7c648f34a6a12e5e8b0063d46f40ea98
Mandar Godale <mandargodale@gmail.com>
2022-03-08 00:30:55 +0530

    added layout view

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = require('./routes/router')
 const port = require('./config/server-config')
+const expressLayouts = require('express-ejs-layouts')
 
 const app = express()
+
+//this needs to be called before routing since views at given route can be using the layout
+app.use(expressLayouts)
 
 //set up the view engine
 app.set('view engine', 'ejs')
