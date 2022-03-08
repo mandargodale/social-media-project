07ac7ece2917fa37d55c7cda648d06520e4ab198
Mandar Godale <mandargodale@gmail.com>
2022-03-09 00:45:06 +0530

    added route for create user and create session

--- routes/user-router.js
+++ routes/user-router.js
@@ -6,5 +6,7 @@ const router = express.Router()
 router.get('/profile', userController.profile)
 router.get('/sign-up', userController.signUp)
 router.get('/sign-in', userController.signIn)
+router.post('/create', userController.create)
+router.post('/create-session', userController.createSession)
 
 module.exports = router
