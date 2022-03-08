b1547e1bc92b72a4bcc23821b2d041ff649a403c
Mandar Godale <mandargodale@gmail.com>
2022-03-08 22:50:28 +0530

    created sign up and sign in form, added create user and create session controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,10 +2,22 @@ module.exports.profile = (req, res) => {
     res.render('profile.ejs', {title: 'Profile'})
 }
 
+//render the sign up page
 module.exports.signUp = (req, res) => {
     res.render('sign-up.ejs', {title: 'Sign Up'})
 }
 
+//render the sign in page
 module.exports.signIn = (req, res) => {
     res.render('sign-in.ejs', {title: 'Sign In'})
+}
+
+//get sign up form data and create user
+module.exports.create = (req, res) => {
+    //TODO
+}
+
+//get sign in form data and create session for the user
+module.exports.createSession = (req, res) => {
+    //TODO
 }
