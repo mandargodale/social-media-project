c15cb892d7a76299eef4159689f6074f43a15773
Mandar Godale <mandargodale@gmail.com>
2022-03-12 14:19:32 +0530

    created comment model, route and controller

---
+++ controllers/comment-controller.js
@@ -0,0 +1,32 @@
+const Comment = require('../models/comment')
+const Post = require('../models/post')
+
+module.exports.create = (req, res) => {
+    const {postId, content} = req.body
+    const {_id} = req.user._id
+
+    //first finding post by if which we are passing from hidden input
+    Post.findById(postId, (err, post) => {
+        if(err) {
+            console.log('error in adding comment since error in finding corresponding post')
+            return
+        }
+        //if post is found
+        if(post) {
+            //create comment, add content, post id and user id in the comment document
+            Comment.create({
+                content: content,
+                post: postId,
+                user: _id
+            }, (err, comment) => {
+                if(err) {
+                    console.log('error in creating comment')
+                    return
+                }
+                post.comments.push(comment)
+                post.save()
+                res.redirect('/')
+            })
+        }
+    })
+}
---
+++ routes/comment-router.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const commentController = require('../controllers/comment-controller')
+const passport = require('passport')
+const passportLocal = require('../config/passport-local-strategy')
+
+const router = express.Router()
+
+//checking if user is signed in before creating post
+router.post('/create', passport.checkAuthentication, commentController.create)
+
+module.exports = router
