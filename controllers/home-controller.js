12435baf909d08cbdddbc5d562d34a4246b17568
Mandar Godale <mandargodale@gmail.com>
2022-03-12 12:42:49 +0530

    displayed all posts on home page

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,3 +1,16 @@
+const Post = require('../models/post')
+const User = require('../models/user')
+
 module.exports.home = (req, res) => {
-    return res.render('home.ejs', {title: 'Home'})
+    Post.find({})
+        //prepoluting posts with users document to display name of the user on post who created it
+        .populate('user')  //this user referes to user property in post document
+        .exec((err, posts) => {
+            if(err) {
+                console.log('error is fetching posts')
+                return
+            }
+            console.log('posts fetched  successfully')
+            return res.render('home.ejs', {title: 'Home', posts})
+        })
 }
