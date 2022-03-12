0c95d56461b60a6e6b2d03881f7e19343d632d84
Mandar Godale <mandargodale@gmail.com>
2022-03-12 14:36:46 +0530

    populated post with comments, displayed comments

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -4,7 +4,15 @@ const User = require('../models/user')
 module.exports.home = (req, res) => {
     Post.find({})
         //prepoluting posts with users document to display name of the user on post who created it
-        .populate('user')  //this user referes to user property in post document
+        .populate('user')  //this user referes to user property in postSchema
+        //prepopulating posts with comments
+        .populate({
+            path: 'comments',
+            //prepopulating comments with users
+            populate: {
+                path: 'user'
+            }
+        })
         .exec((err, posts) => {
             if(err) {
                 console.log('error is fetching posts')
