2919afb84677843805fc01854a486f99db350692
Mandar Godale <mandargodale@gmail.com>
2022-03-12 12:57:51 +0530

    added authentication for creating post

--- routes/post-router.js
+++ routes/post-router.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const postController = require('../controllers/post-controller')
+const passport = require('passport')
+const passportLocal = require('../config/passport-local-strategy')
 
 const router = express.Router()
 
-router.post('/create', postController.create)
+//checking if user is signed in before creating post
+router.post('/create', passport.checkAuthentication, postController.create)
 
 module.exports = router
