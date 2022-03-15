c5907101deda3fbc20c6133e0fc152ce54f77c61
Mandar Godale <mandargodale@gmail.com>
2022-03-15 23:08:27 +0530

    added AJAX to display created post on UI

--- public/js/post.js
+++ public/js/post.js
@@ -1,5 +1,5 @@
 //method to submit form data to post-controller.js create() for creating new post
-const createPost = function () {
+const createPost = () => {
     console.log('Inside createPost()')
     const createPostForm = $('#create-post-form')
     createPostForm.submit((e) => {
@@ -10,13 +10,42 @@ const createPost = function () {
             url: '/post/create',
             data: createPostForm.serialize(),  //it converts form data into JSON
             success: (data) => {
-                console.log(data)
+                const createdPost = appendPost(data.data.post)
+                $('#post-list-container>ul').prepend(createdPost)
             },
             error: (error) => {
                 console.log(error.responseText)
             }
         })
     })
+}
+
+//method to append the created post to DOM
+const appendPost = (post) => {
+    return $(`
+    <li id="post-${post._id}">
+        <p>
+            <small>
+                <a href="/post/destroy/${post._id}" class="delete-post-button">X</a>
+            </small>
+            ${post.content}
+            <br>
+            <small>
+            ${post.user.name}
+            </small>
+        </p>
+        <div id="post-comment">
+            <form action="/comment/create" method="post">
+                <input type="text" name="content" placeholder="Add comment..." required>
+                <input type="hidden" name="postId" value="${post._id}">
+                <button>Add comment</button>
+            </form>
+            <div id="post-comment-list">
+                <ul id="post-comments-${post._id}">
+                </ul>
+            </div>
+        </div>
+    </li>`)
 }
 
 createPost()
