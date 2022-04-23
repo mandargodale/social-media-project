b33382b229b545fdcc15be6d5bec8a1225283147
Mandar Godale <mandargodale@gmail.com>
2022-04-24 01:29:19 +0530

    delete controller api part

--- routes/router.js
+++ routes/router.js
@@ -4,7 +4,6 @@ const userRouter = require('./user-router')
 const postRouter = require('./post-router')
 const commentRouter = require('./comment-router')
 const likeRouter = require('./like-router')
-const api = require('./api/api-router')
 
 const router = express.Router()
 
@@ -14,7 +13,5 @@ router.use('/user', userRouter)
 router.use('/post', postRouter)
 router.use('/comment', commentRouter)
 router.use('/like', likeRouter)
-
-router.use('/api', api)
 
 module.exports = router
